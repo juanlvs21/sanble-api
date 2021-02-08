51abe97eabd9183315b8520486878fdcd7f300f3
Juan Villarroel <juanlvs97@gmail.com>
2021-02-08 16:10:35 -0400

    Configure send email Recover-Password

--- utils/sgMain.ts
+++ utils/sgMain.ts
@@ -4,6 +4,7 @@ import { SENDGRID_KEY, SENDGRID_DOMAIN } from "./env";
 
 // Templates
 import welcome from "../templates/welcome";
+import recoverPassword from "../templates/recoverPassword";
 
 sgMail.setApiKey(SENDGRID_KEY);
 
@@ -13,6 +14,29 @@ export const welcomeEmail = (to: string, displayName: string, link: string) => {
     from: `Equipo Sanble <welcome${SENDGRID_DOMAIN}>`,
     subject: "Bienvenido a Sanble",
     html: welcome(displayName, link),
+  };
+
+  return new Promise(async (resolve, rejects) => {
+    await sgMail
+      .send(msg)
+      .then(() => resolve("Email sent successfully"))
+      .catch((error) => {
+        if (error.response) rejects(error.response.body.errors);
+        else rejects(error);
+      });
+  });
+};
+
+export const recoverPasswordEmail = (
+  to: string,
+  displayName: string,
+  link: string
+) => {
+  const msg = {
+    to,
+    from: `Equipo Sanble <support${SENDGRID_DOMAIN}>`,
+    subject: "¿Problemas para entrar?",
+    html: recoverPassword(displayName, link),
   };
 
   return new Promise(async (resolve, rejects) => {
