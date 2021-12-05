2c22d77c69319df16c308657897bde4635895e4b
Juan Villarroel <juanlvs97@gmail.com>
2021-12-05 14:36:42 -0400

    modified:   src/routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
-import { Router } from "express";
+import { Router, Response } from "express";
 
 import authRoutes from "@/routes/auth.routes";
 
 const routes = Router();
+
+routes.get("/", (_, res: Response) => {
+  res.status(200).json({ data: "Welcome to de API" });
+});
 
 routes.use("/auth", authRoutes);
 
