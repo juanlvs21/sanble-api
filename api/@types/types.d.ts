75f71009c173cd740fbb434376e54bd957aee82c
Juan Villarroel <juan@matic.io>
2022-03-06 12:59:10 -0400

    Clear old files

--- api/@types/types.d.ts
+++
@@ -1,7 +0,0 @@
-type TResponse = import("express").Response;
-
-type Handler = (
-  req: import("express").Request,
-  res: TResponse,
-  next: import("express").NextFunction
-) => Promise<TResponse | void> | TResponse | void;
--- api/utils/getHost.ts
+++
@@ -1,5 +0,0 @@
-import { PORT } from "../config/env";
-
-export const getAppDomain = (host: string): string => {
-  return host.includes("localhost:") ? `http://localhost:${PORT}` : host;
-};
