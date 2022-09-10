3671c28fdec13740ff0fa56c598201c7349fcbdc
Juan Villarroel <juanlvs97@gmail.com>
2022-09-10 15:44:43 -0400

    Add healthcheck path

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,4 +10,7 @@ router.use("/auth", authRoutes);
 router.use("/fairs", fairRoutes);
 router.use("/stands", standRoutes);
 
+router.get("/ping", (_req, res) => res.status(200).send("pong"));
+router.get("/healthcheck", (_req, res) => res.status(200).send("pong"));
+
 export default router;
