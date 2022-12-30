08ea0cca3c918ba4231b6fa27f958a33a908ae15
Juan Villarroel <juanlvs97@gmail.com>
2022-12-30 11:40:23 -0400

    modified:   src/utils/firebase.ts

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,12 +4,16 @@ import { getFirestore } from "firebase-admin/firestore";
 export * from "firebase-admin/auth";
 export * from "firebase-admin/firestore";
 
-import serviceAccountJSON from "../../firebase-adminsdk.json";
+import {
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_PROJECT_ID,
+} from "../config/env";
 
 const serviceAccount: ServiceAccount = {
-  projectId: serviceAccountJSON.project_id,
-  clientEmail: serviceAccountJSON.client_email,
-  privateKey: serviceAccountJSON.private_key,
+  projectId: FIREBASE_PROJECT_ID,
+  privateKey: FIREBASE_PRIVATE_KEY,
+  clientEmail: FIREBASE_CLIENT_EMAIL,
 };
 
 export const app = initializeApp({
