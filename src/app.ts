e7f1b07336c16aeaae84999ce6796c7704e2be2b
Juan Villarroel <juanlvs97@gmail.com>
2023-02-04 15:22:41 -0400

    modified:   src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.set("port", PORT);
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", routes);
 app.use(handleError);
