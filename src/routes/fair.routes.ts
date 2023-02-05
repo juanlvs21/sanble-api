b8625a71f3c4eb7b20dae7d1b2676f8473f9e198
Juan Villarroel <juanlvs97@gmail.com>
2023-02-05 09:32:31 -0400

    Small changes

--- src/routes/fair.routes.ts
+++ src/routes/fair.routes.ts
@@ -1,7 +1,6 @@
 import { FairController } from "../controllers/fair.controller";
 import { ErrorRouter } from "../error";
 import { sessionMiddleware } from "../middlewares/session.middleware";
-import { fairPhotographValidator } from "../validators/fair.validators";
 import { reviewValidator } from "../validators/review.validators";
 
 const router = new ErrorRouter();
