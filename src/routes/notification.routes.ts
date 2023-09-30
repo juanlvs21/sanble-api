cb6a0e3d5bf547c21fb141d2e672fc02ad5ebf13
Juan Villarroel <juanlvs97@gmail.com>
2023-09-30 09:43:29 -0400

    Add sessionMiddleware in post /notifications/token

--- src/routes/notification.routes.ts
+++ src/routes/notification.routes.ts
@@ -1,11 +1,13 @@
 import { NotificationController } from "../controllers/notification.controller";
 import { ErrorRouter } from "../error";
+import { sessionMiddleware } from "../middlewares/session.middleware";
 import { tokenNotificationValidator } from "../validators/notification.validator";
 
 const router = new ErrorRouter();
 
 router.post(
   "/token",
+  sessionMiddleware,
   tokenNotificationValidator,
   NotificationController.saveToken
 );
